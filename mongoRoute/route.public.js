e5f8abc996acf4d29900f40924d319bcab3bd982
himanshubhatt02 <himanshubhatt202@gmail.com>
2022-06-08 09:50:59 -0700

    about vaccination

--- mongoRoute/route.public.js
+++ mongoRoute/route.public.js
@@ -3,5 +3,8 @@ const router = express.Router()
 router.get("/", (req, res, next) => {
   res.render("index")
 })
+router.get("/aboutvaccine", async (req, res, next) => {
+  res.render("aboutvaccine") //for rendering doctor signup
+})
 
 module.exports = router
