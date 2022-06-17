8189a60063bb0340f761e1294f2c848f838e6f19
himanshubhatt02 <himanshubhatt202@gmail.com>
2022-06-17 11:56:05 -0700

    port change

--- server.js
+++ server.js
@@ -175,7 +175,7 @@ app.use(
 // })
 
 //Initializing port
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
